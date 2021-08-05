a21f46c986ef04b28c93b163d5a347bf13e7f579
tosanoke <tosanoke@gmail.com>
2021-08-05 18:49:00 +0100

    maps update

---
+++ script.js
@@ -0,0 +1,37 @@
+
+
+$(document).ready(function() {
+        
+  /* activate the carousel */
+  $("#carouselExampleIndicators").carousel({interval:false});
+
+  /* change modal title when slide changes */
+  $("#carouselExampleIndicators").on("slid.bs.carousel", function () {
+       $(".modal-title")
+       .html($(this)
+       .find(".active img")
+       .attr("title"));
+  });
+
+  /* when clicking a thumbnail */
+  $(".row .view-map").click(function(){
+   var content = $(".carousel-inner");
+   var title = $(".modal-title");
+ 
+   content.empty();  
+   title.empty();
+ 
+    var id = this.id;  
+    var repo = $("#img-repo .item");
+    var repoCopy = repo.filter("#" + id).clone();
+    var active = repoCopy.first();
+ 
+   active.addClass("active");
+   title.html(active.find("img").attr("title"));
+     content.append(repoCopy);
+
+   // show the modal
+     $("#modal-gallery").modal("show");
+ });
+
+});
